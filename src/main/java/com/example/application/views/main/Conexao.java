b5ff8a6fd14a1a3c5ba2b0275a5109053c36ddd0
LopesLeandro <src.leandro@gmail.com>
2023-09-22 23:41:23 -0300

    Conexao com banco de dados configurada_JDBC

---
+++ src/main/java/com/example/application/views/main/Conexao.java
@@ -0,0 +1,36 @@
+package com.example.application.views.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static void main(String[] args) {
+        Connection conexao = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/BancoFinanceiro", "root", "@Ruth12345");
+            ResultSet rsBanco = conexao.createStatement().executeQuery("SELECT * FROM Gastos");
+            while (rsBanco.next()) {
+                System.out.println(rsBanco.getString("tipo"));
+            }
+            System.out.println("Driver do banco de dados localizado");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver do banco de dados não localizado");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Ocorreu outro erro ao tentar conectar ao banco de dados: " + e.getMessage());
+        } finally {
+            try {
+                if (conexao != null && !conexao.isClosed()) {
+                    conexao.close();
+                    System.out.println("Conexão ao banco de dados encerrada");
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro ao encerrar a conexão: " + e.getMessage());
+            }
+        }
+    }
+}
+
